a5298c9ca8e4dde50e7cf0ab6c523f55587464e1
Gülsüm <glsmbyrkdrr@gmail.com>
2026-05-20 12:37:36 +0300

    Konu secimi yapıldı ilk kod eklendi

---
+++ README.md
@@ -0,0 +1,5 @@
+# E-Ticaret Sepeti Kampanya Sistemi (Konu D)
+
+Gerekçe: Gerçek dünya projelerinde kampanya ve indirim kuralları sürekli değişiklik gösterir. 
+Bu kuralları sepet sınıfına gömerek yazmak, Open-Closed prensibini doğrudan ihlal eder. 
+Bu acıyı deneyimlemek ve doğru tasarımı uygulamak için bu konuyu seçtim.
---
+++ src/sepet.py
@@ -0,0 +1,15 @@
+
+class AlisverisSepeti:
+    def __init__(self, toplam_tutar):
+        self.toplam_tutar = toplam_tutar
+
+    def toplam_hesapla(self, indirim_turu):
+        if indirim_turu == "EFSANE_KASIM":
+            return self.toplam_tutar * 0.50  # %50 indirim
+        elif indirim_turu == "HOŞ_GELDİN_KUPONU":
+            return self.toplam_tutar - 50     # 50 TL indirim
+        elif indirim_turu == "ÖĞRENCİ":
+            return self.toplam_tutar * 0.90  # %10 indirim
+        else:
+            return self.toplam_tutaro
+
